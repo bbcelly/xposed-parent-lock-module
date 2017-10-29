d1dcbfb644290fd5e66e42bfc449cc6de01718d5
jarin <jarin@livesport.eu>
2017-10-29 13:28:48 +0100

    app info service

--- app/src/main/java/cz/jarin/parentlock/services/GetAppInfoService.java
+++ app/src/main/java/cz/jarin/parentlock/services/GetAppInfoService.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import cz.jarin.parentlock.utils.StorageAppLock;
+import cz.jarin.parentlock.utils.StorageAppLockPrefs;
 import de.robv.android.xposed.XposedBridge;
 
 /**
@@ -16,6 +18,8 @@ import de.robv.android.xposed.XposedBridge;
 public class GetAppInfoService extends IntentService {
 
 	private static final String TAG = "GetAppInfoService";
+
+	private static StorageAppLock<String, Integer> storageAppLock;
 
 	public static class Receiver extends BroadcastReceiver {
 
@@ -34,10 +38,25 @@ public class GetAppInfoService extends IntentService {
 
 	public GetAppInfoService() {
 		super("GetAppInfoService");
+		storageAppLock = new StorageAppLockPrefs(getApplicationContext());
 	}
 
 	@Override
 	protected void onHandleIntent(@Nullable Intent intent) {
 		Log.d(TAG, "onHandleIntent: ");
+		if (intent == null) {
+			return;
+		}
+		boolean isAppOk = true;
+		String appPackage = intent.getStringExtra("packageName");
+		if (storageAppLock.isEnabledAppLock(appPackage)) {
+			int timeSec = Math.round(intent.getLongExtra("time", 0) / 1000f);
+			int timeRemainingSeconds = storageAppLock.getRemainingTimePerDay(appPackage) - timeSec;
+			if (timeRemainingSeconds <= 0) {
+				timeRemainingSeconds = 0;
+				isAppOk = false;
+			}
+			storageAppLock.setRemainingTimePerDay(appPackage, timeRemainingSeconds);
+		}
 	}
 }
